0e28ab783f68df4db17883e6f6337849635f665f
Slavic1ouchakov <slavic.ouchakov@gmail.com>
2021-07-14 22:16:50 -0400

    Added validation

--- Develop/index.js
+++ Develop/index.js
@@ -10,52 +10,62 @@ const questions = [
     type: "input",
     message: "What is your project title?",
     name: "title",
+    validate: (value) => { if (value) { return true } else { return 'Please provide value to continue' } }
 },
 {
     type: "input",
-    message: "Describe your project",
+    message: "Describe your project.",
     name: "description",
+    validate: (value) => { if (value) { return true } else { return 'Please provide value to continue' } }
 },
 {
     type: "input",
     message: "Installation",
     name: "installation",
+    validate: (value) => { if (value) { return true } else { return 'Please provide value to continue' } }
 },
 {
     type: "input",
-    message: "Usage",
+    message: "Usage of application.",
     name: "usage",
+    validate: (value) => { if (value) { return true } else { return 'Please provide value to continue' } }
 },
 {
     type: "list",
-    message: "Choose the License",
+    message: "Choose the applicable License.",
     name: "license",
-    choices: ["MIT license","Apache License 2.0","GNU General pablic license v3.0"] 
+    choices: ["MIT license","Apache License 2.0","GNU General public license v3.0"],
+    validate: (value) => { if (value) { return true } else { return 'Please provide value to continue' } }
 },
 {
     type: "input",
-    message: "Contributing",
+    message: "Who contributed?",
     name: "Contributing",
+    validate: (value) => { if (value) { return true } else { return 'Please provide value to continue' } }
 },
 {
     type: "input",
-    message: "tests",
+    message: "Steps to test application.",
     name: "tests",
+    validate: (value) => { if (value) { return true } else { return 'Please provide value to continue' } }
 },
 {
     type: "input",
-    message: "Enter your GitHub user?",
+    message: "Enter your GitHub user.",
     name: "GitHubuser",
+    validate: (value) => { if (value) { return true } else { return 'Please provide value to continue' } }
 },
 {
     type: "input",
     message: "Enter your email address",
     name: "email",
+    validate: (value) => { if (value) { return true } else { return 'Please provide value to continue' } }
 },
 {
     type: "input",
     message: "Enter your project's GitHub repository",
     name: "repository",
+    validate: (value) => { if (value) { return true } else { return 'Please provide value to continue' } }
 },
 ];
 
@@ -65,7 +75,7 @@ function writeToFile(fileName, data) {
         if (err) {
           return console.log(err);
         }
-        console.log("Success!");
+        console.log("Success! Your README was generated!");
       }); 
 }
 
